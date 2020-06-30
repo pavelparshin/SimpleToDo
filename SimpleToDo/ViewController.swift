24e2470517e8d332ccf6112c68a3eb95b2a656ec
Pavel Parshin <mail@pavelparshin.ru>
2020-06-30 11:24:04 +0300

    Initial Commit

---
+++ SimpleToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleToDo
+//
+//  Created by Pavel Parshin on 30.06.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
