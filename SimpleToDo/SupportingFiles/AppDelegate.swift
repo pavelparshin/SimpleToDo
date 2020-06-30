7bf5c413c1cd8336254e0c2283a08b7a447a8c9a
Pavel Parshin <mail@pavelparshin.ru>
2020-06-30 17:02:10 +0300

    update file's structure

---
+++ SimpleToDo/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  SimpleToDo
+//
+//  Created by Pavel Parshin on 30.06.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = UINavigationController(rootViewController: TaskListViewController())
+        
+        return true
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        StorageManager.shared.saveContext()
+    }
+}
+
